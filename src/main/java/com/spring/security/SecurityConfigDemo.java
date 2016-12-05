0835c8c7c2ff053fb38e2ad2051f925a3f40a57c
Dishari <dishari@thrymr.net>
2016-12-05 16:48:20 +0530

    Check in token based auth

--- src/main/java/com/spring/security/SecurityConfigDemo.java
+++ src/main/java/com/spring/security/SecurityConfigDemo.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -18,13 +19,16 @@ public class SecurityConfigDemo extends WebSecurityConfigurerAdapter {
 	private DemoAuthenticationProvider demoAuthenticationProvider;
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/hello/**").permitAll().anyRequest().authenticated().and()
-				.addFilterBefore(new DemoAuthenticationFilter(), BasicAuthenticationFilter.class);
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/hello/*");
+	}
 
-		http.exceptionHandling().and().anonymous().and().servletApi().and().authorizeRequests().antMatchers("/hello/hw")
-				.permitAll().anyRequest().authenticated().and()
-				.addFilterBefore(new DemoAuthenticationFilter(), BasicAuthenticationFilter.class);
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().anyRequest().authenticated().and().addFilterBefore(new DemoAuthenticationFilter(),
+				BasicAuthenticationFilter.class);
+
 	}
 
 	@Override
